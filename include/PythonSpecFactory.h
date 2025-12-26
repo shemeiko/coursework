f193991c758763b4a40a2b914d3dfe4c8b8f1729
Shemeiko Taras <shemiks2005@gmail.com>
2025-12-26 20:44:54 +0200

    refactored

--- include/PythonSpecFactory.h
+++ include/PythonSpecFactory.h
@@ -10,17 +10,11 @@ public:
             "False", "None", "True",
             "and", "as", "assert", "async", "await",
             "break", "class", "continue",
-            "def", "del",
-            "elif", "else", "except",
+            "def", "del", "elif", "else", "except",
             "finally", "for", "from",
-            "global",
-            "if", "import", "in", "is",
-            "lambda",
-            "nonlocal", "not",
-            "or",
-            "pass",
-            "raise", "return",
-            "try",
+            "global", "if", "import", "in", "is",
+            "lambda", "nonlocal", "not", "or",
+            "pass", "raise", "return", "try",
             "while", "with", "yield"
         };
     }
